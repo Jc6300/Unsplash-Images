da5b8a3be727f2c7f69eaf7e27de76cb90950751
Jc6300 <joshcotton93@gmail.com>
2023-11-11 19:20:37 -0600

    updated app

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,7 +4,7 @@ import { useGlobalContext } from "./context";
 const url = `https://api.unsplash.com/search/photos?client_id=${
 	import.meta.env.VITE_API_KEY
 }`;
-console.log(import.meta.env.VITE_API_KEY);
+
 const Gallery = () => {
 	const { searchTerm } = useGlobalContext();
 	const response = useQuery({
